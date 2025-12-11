6be2576c2e0fc942e9b4187ec8162c74cce7031c
AshDev0 <college.ashwani@gmail.com>
2025-12-11 17:31:24 +0530

    Initial commit: Complete website setup

--- 100devprojects/src/App.jsx
+++ 100devprojects/src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import ProjectPage from './pages/ProjectPage'
-import Header from './components/Header'
-import Footer from './components/Footer'
-export default function App() {
-return (
-<BrowserRouter>
-<div className="min-h-screen flex flex-col">
-<Header />
-<main className="flex-1 container mx-auto px-4 py-6">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/projects" element={<Projects />} />
-<Route path="/projects/:slug" element={<ProjectPage />} />
-</Routes>
-</main>
-<Footer />
-</div>
-</BrowserRouter>
-)
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ProjectDetail from './pages/ProjectDetail';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/project/:slug" element={<ProjectDetail />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
+
+export default App;
